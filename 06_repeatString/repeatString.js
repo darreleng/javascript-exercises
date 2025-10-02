735908bc93624b69d78567753b6c9e9846ff48f7
Darrel <128795218+darreleng@users.noreply.github.com>
2025-10-02 17:41:57 +0800

    Finished some javascript exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(streng, nume) {
+    if (nume < 0) return "ERROR";
+    let word = "";
+    for (let i = 0; i < nume; i++) {
+        word += streng;
+    }
+    return word;
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let output = "";
+        
+    for (let x = input.length - 1; x >= 0; x--) { 
+        output += input[x]; 
+    }
+    return output; 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
